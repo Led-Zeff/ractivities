c68b4dd009591c0beeff5778e2a5274e2695549f
Led-Zeff <ortega.victor.t.v@gmail.com>
2020-12-07 14:56:44 -0600

    Values controller

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,25 +1,41 @@
 using System.Collections.Generic;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ValuesController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ValuesController : ControllerBase
+  {
+    private readonly DataContext _dataContext;
+
+    public ValuesController(DataContext dataContext)
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> GetAction() {
-            return new[] {"Value 1", "Value 2", "Value 3"};
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<string> GetAction(int id) {
-            return "Value " + id;
-        }
-
-        [HttpPost]
-        public void Post([FromBody] string value) {
-
-        }
+        this._dataContext = dataContext;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Value>>> Get()
+    {
+        var values = await _dataContext.Values.ToListAsync();
+        return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Value>> Get(int id)
+    {
+        var value = await _dataContext.Values.FindAsync(id);
+        return Ok(value);
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+
+    }
+  }
 }
